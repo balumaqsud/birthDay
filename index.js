731f63a84d8d3ba14f0c2f3c58482792364f9977
balumaqsud <ollobergan008@mail.ru>
2025-02-14 17:36:30 +0900

    updated

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const moment = require("moment"); //moment
-// const today = moment().format("[today is: ] YYYY MM DD[th]");
-// console.log(today);
+import moment from "moment";
+import inquirer from "inquirer";
 
-let today = moment([2025, 2, 13]);
-let birthDate = moment([2000, 7, 25]);
+// let today = moment([2025, 2, 13]);
+// let birthDate = moment([2000, 7, 25]);
 
-// let years = today.diff(birthDate, "years");
-// birthDate.add(years, "years");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "date",
+      message: "what is the date of your birthday?: in YYYY MM DD",
+    },
+  ])
+  .then((answer) => {
+    let date = answer.date.split(" ");
+    let bDay = moment(date.join("-", "YYYY-MM-DD"));
+    let today = moment();
+    let differ = moment.duration(today.diff(bDay));
+    console.log(`${differ.years()} ${differ.months()} ${differ.days()}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-// let months = today.diff(birthDate, "months");
-// birthDate.add(months, "months");
-
-// let days = today.diff(birthDate, "days");
-// console.log(`${years} ${months} ${days}`);
-
-let timePassed = moment.duration(today.diff(birthDate));
-console.log(
-  `${timePassed.years()} ${timePassed.months()} ${timePassed.days()}`
-);
+// function toNumbers(list) {
+//   let numbers = [];
+//   for (let i = 0; i < list.length; i++) numbers.push(parseInt(list[i]));
+//   return numbers;
+// }
